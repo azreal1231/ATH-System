e915c4fbfb7f9f391b55258d699041f9bf56a6cd
Azreal <edwardjack91@gmail.com>
2020-12-24 18:06:02 +0200

    ? Started with vehicle.py

--- DataModels/vehicle.py
+++ DataModels/vehicle.py
@@ -1,3 +1,15 @@
 from couchdb.mapping import *
 
-class Clinic(Document):
+
+class vehicle(Document):
+    REGISTRATION_PLATE = TextField(default='')
+
+    lAST_USERS = TextField(default='')
+
+    CURRENT_USERS = TextField(default='')
+
+    VE = DictField(default={'': {'Amount': ''}, '': {'Amount': ''}})
+
+    MSE = DictField(default={'': {'Amount': ''}, '': {'Amount': ''}})
+
+    PPE = DictField(default={'': {'Amount': ''}, '': {'Amount': ''}})
